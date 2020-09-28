c88c78ad771c16dbb54ee972cc4d347d61d45027
Rzky <48115312+rzii@users.noreply.github.com>
2020-09-28 12:39:49 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => 'db-pilketos.mysql.database.azure.com',
+	'username' => 'dbpilketos@db-pilketos',
+	'password' => 'Pilketoshokyahokya!123',
 	'database' => 'pilketos',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
