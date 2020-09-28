e619ad7d287a6e6761a7bc8b1cf06ce20eefbe8a
Rzky <48115312+rzii@users.noreply.github.com>
2020-09-28 12:38:13 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/e-pilketos/';
+$config['base_url'] = ''. $_SERVER['HTTP_HOST'] .'';
 
 /*
 |--------------------------------------------------------------------------
